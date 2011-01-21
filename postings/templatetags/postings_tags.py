189b4426d72708d46f2a836ae2292c6504b74b63
Philipp Wassibauer <phil@maptales.com>
2011-01-21 22:36:07 +0100

    added share functionality

--- postings/templatetags/postings_tags.py
+++ postings/templatetags/postings_tags.py
@@ -4,8 +4,15 @@ from django.contrib.contenttypes.models import ContentType
 
 register = Library()
 
+
 @register.inclusion_tag("postings/posting_form.html")
 def posting_form(object):
     # check if following already
+    ctype = ContentType.objects.get_for_model(object)
+    return {"content_type": ctype.pk, "object_id":object.pk}
+
+
+@register.inclusion_tag("postings/share.html")
+def posting_share(object):
     ctype = ContentType.objects.get_for_model(object)
     return {"content_type": ctype.pk, "object_id":object.pk}
